1058bb8dfe89d73be5f9f6dec65fabdb13530a2a
peogway <duchungbeo2004@gmail.com>
2025-08-02 20:08:47 +0300

    save best wave

--- Scripts/Global.gd
+++ Scripts/Global.gd
@@ -1,11 +1,13 @@
 extends Node
 
-var player_mode = 1# 1 or 2
+var player_mode = 2# 1 or 2
 var selected_character = 2
 var best_score = 0
 var best_kills = 0
 var best_coins = 0
 var best_time = 0
+var best_wave = 1
+
 var music = false
 var sound = false
 
@@ -25,6 +27,7 @@ func save_sound_setting():
 		"music": music,
 		"sound": sound,
 		"score": best_score,
+		"wave": best_wave,
 		"time": best_time,
 		"kills": best_kills,
 		"coins": best_coins
@@ -35,8 +38,9 @@ func save_sound_setting():
 		file.close()
 
 
-func save_score_setting(score: int, time: float, kills: int, coins: int):
+func save_score_setting(score: int, wave:int, time: float, kills: int, coins: int):
 	best_score = score
+	best_wave = wave
 	best_time = time
 	best_kills = kills
 	best_coins = coins
@@ -44,9 +48,10 @@ func save_score_setting(score: int, time: float, kills: int, coins: int):
 		"music": music,
 		"sound": sound,
 		"score": score,
+		"wave": wave,
 		"time": time,
 		"kills": kills,
-		"coins": coins
+		"coins": coins,
 	}
 	var file = FileAccess.open("user://settings.save", FileAccess.WRITE)
 	if file:
